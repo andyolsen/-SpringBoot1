26294a8d884601443c8ae40967975ace603d699f
Petter Tondel <petter.toendel@outlook.com>
2021-03-15 13:50:55 +0100

    Oppsett av Maven og Spring Boot repo

---
+++ online-retailer2/src/main/java/no/dnb/reskill/onlineretailer2/OnlineRetailer2Application.java
@@ -0,0 +1,13 @@
+package no.dnb.reskill.onlineretailer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRetailer2Application.class, args);
+    }
+
+}
